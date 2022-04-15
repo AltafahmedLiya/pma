508b27c19fecfb5f5bdef4e2a84c6311e193d472
AltafLiya <daduliya251@gmail.com>
2022-04-15 08:58:45 +0530

    Completed WoS&S

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 from email.policy import default
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, flash, render_template, url_for, request, redirect,session
+from flask import Flask, flash, render_template, url_for, request, redirect,session, abort
 from forms import RegistrationForm, LoginForm, PostForm
 from flask_bcrypt import Bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
-from flask_login import LoginManager
+from flask_login import LoginManager, UserMixin
 import sqlite3
 import os
 app = Flask(__name__)
@@ -21,9 +21,9 @@ login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.get(user_id)
+    return User.query.get(int(user_id))
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -45,23 +45,10 @@ class Post(db.Model):
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
 
-posts = [
-    {
-        'builder': 'Altaf',
-        'property':'Flat',
-        'details':'56sqft',
-        'date_posted':'April 10 2022'
-    },
-    {
-        'builder': 'Ahmed',
-        'property':'plot',
-        'details':'100sqft',
-        'date_posted':'April 20 2022'
-    }
-]
 @app.route('/',methods=['GET','POST'])
 def home():
-    return render_template('home.html',posts=posts)
+    posts = Post.query.order_by(Post.date_posted.desc()).all()
+    return render_template('home.html', posts=posts)
 
 @app.route('/register', methods=['GET','POST'])
 def register():
@@ -104,9 +91,48 @@ def forgot():
 def new_post():
         form = PostForm()
         if form.validate_on_submit():
+            post = Post(title=form.title.data, content=form.content.data, author=current_user)
+            db.session.add(post)
+            db.session.commit()
             flash('Your post has been created!','success')
             return redirect(url_for('home'))
-        return render_template('/create_post.html', title='New Post', form=form)
+        return render_template('create_post.html', title='New Post', form=form, legend = 'New Post')
         
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post',
+                           form=form, legend='Update Post')
+
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
+
 if __name__ == "__main__":
     app.run(debug=True)
